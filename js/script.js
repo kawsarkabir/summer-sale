1b0378a4bd1f2d8a5f9d35fcfd5db0bd6fc681fb
kawsarkabir <devkawsarkabir@gmail.com>
2023-08-21 16:00:58 +0600

    summer sale assingment 5 done

--- js/script.js
+++ js/script.js
@@ -21,6 +21,7 @@ function handleClick(target) {
   const totalElement = document.getElementById("total");
   totalElement.innerText = sumOfTotal.toFixed(2);
 
+  // make purchase and coupon button disable enable here
   const makePurchase = document.getElementById("make-purchase");
   const couponBtn = document.getElementById("coupon-btn");
   if (sumOfTotal > 0) {
@@ -38,20 +39,32 @@ function handleClick(target) {
   couponBtn.addEventListener("click", applyCouponCode);
   function applyCouponCode() {
     const couponCode = "SELL200";
-    
+
     if (couponCode === "SELL200") {
-        // Calculate the discounted price
-        const discountPercentage = 20; // 20% discount
-        const discountAmount = (discountPercentage / 100) * sumOfTotal;
-        const discountPriceElement = document.getElementById('discount-price');
-        discountPriceElement.innerText = discountAmount.toFixed(2);
-        const discountedTotal = sumOfTotal - discountAmount;
-        
-        // Update the displayed total with the discounted amount
-        const totalPriceElement = document.getElementById('total');
-        totalPriceElement.innerText = discountedTotal.toFixed(2);
+      // Calculate the discounted price
+      const discountPercentage = 20; // 20% discount
+      const discountAmount = (discountPercentage / 100) * sumOfTotal;
+      const discountPriceElement = document.getElementById("discount-price");
+      discountPriceElement.innerText = discountAmount.toFixed(2);
+      const discountedTotal = sumOfTotal - discountAmount;
+
+      // Update the displayed total with the discounted amount
+      const totalPriceElement = document.getElementById("total");
+      totalPriceElement.innerText = discountedTotal.toFixed(2);
     } else {
-        alert("Invalid coupon code.");
+      alert("Invalid coupon code.");
     }
-}
+  }
+
+  // modal click btn then refresh the page
+  const makePurchaseButton = document.getElementById("make-purchase");
+  const homeButton = document.querySelector("#my_modal_1 button");
+
+  makePurchaseButton.addEventListener("click", function() {
+    my_modal_1.showModal();
+  });
+
+  homeButton.addEventListener("click", function() {
+    location.reload();
+  });
 }
